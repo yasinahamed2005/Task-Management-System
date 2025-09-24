69a64f6fe9eb28029ccb45e2438611826c1d8a28
Yasinahamad <163731345+yasinahamed2005@users.noreply.github.com>
2025-09-24 21:39:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port:3000
-  }
+  base: '/Task-Management-System/',   // 👈 IMPORTANT
 })
